6332ac779475da395be6c371cae23bf5e0894056
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-28 15:22:35 -0700

    added eventlitener

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // import functions and grab DOM elements
-
+const climateDiv = document.getElementById('Climate button');
+const architectureDiv = document.getElementById('Architecture button');
 // let state
-
+let Climate = 0;
+let Architecture = 0;
 // set event listeners 
+climateDiv.addEventListener('change', () => {
+  
+})
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
