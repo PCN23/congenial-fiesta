d47c922cbf37585568ba9660a525aee9915f6f51
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-28 15:36:21 -0700

    added const functions

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // import functions and grab DOM elements
-const climateDiv = document.getElementById('Climate button');
-const architectureDiv = document.getElementById('Architecture button');
+const climateSelect = document.getElementById('climate-select');
+const climatePicture = document.getElementById('climate-pic');
+
+const architectureSelect = document.getElementById('architecture-select');
+const architecturePicture = document.getElementById('architecture-pic');
 // let state
 let Climate = 0;
 let Architecture = 0;
 // set event listeners 
 climateDiv.addEventListener('change', () => {
-  
+  console.log('', climateDiv.value);
+})
+
+architectureDiv.addEventListener('change', () => {
+
 })
   // get user input
   // use user input to update state 
