af200b6ce209ab215b37b5505dff4fe56001e6e7
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-29 11:38:12 -0700

    added slogan

--- app.js
+++ app.js
@@ -10,36 +10,38 @@ const sloganInput = document.getElementById('slogan-input');
 // let state
 let climate = 0;
 let architecture = 0;
-let slogans = [];
+let coastline = 0;
+
+let slogan = [];
 // set event listeners 
 climateSelect.addEventListener('change', () => {
-  console.log('changing climate', climateSelect.value);
-  climatePhoto.src = `..assets/${climateSelect.value}-climate.png`;
-  climatePhoto.style.backgroundImage = `url('../assets/${climateSelect.value}-climate.png')`;
-  climate++;
-})
+    console.log('changing climate', climateSelect.value);
+    climatePhoto.src = `..assets/${climateSelect.value}-climate.png`;
+    climatePhoto.style.backgroundImage = `url('../assets/${climateSelect.value}-climate.png')`;
+    climate++;
+});
 
 architectureSelect.addEventListener('change', () => {
-  console.log('changing architecture', architectureSelect.value);
-  architecturePhoto.src = `..assets/${climateSelect.value}-climate.png`;
-  architecturePhoto.style.backgroundImage = `url('../assets/${architecturePhoto.value}-architecture.png')`;
-  architecture++;
-})
+    console.log('changing architecture', architectureSelect.value);
+    architecturePhoto.src = `..assets/${climateSelect.value}-climate.png`;
+    architecturePhoto.style.backgroundImage = `url('../assets/${architecturePhoto.value}-architecture.png')`;
+    architecture++;
+});
 
-slogan.addEventListener('click', () => {
-  slogan.push(sloganInput.value);
-  displaySlogan();
+sloganInput.addEventListener('click', () => {
+    slogan.push(sloganInput.value);
+    displaySlogans();
 
-})
+});
 
 function displaySlogans() {
-  sloganList.textContent = '';
-  for (let slogan of slogans) {
-    const p = document.createElement ('p');
-    p.textContent = slogan;
-    slogan.append(p);
+    sloganInput.textContent = '';
+    for (let slogan of slogans) {
+        const p = document.createElement ('p');
+        p.textContent = slogan;
+        slogan.append(p);
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-  }
+    }
 }
