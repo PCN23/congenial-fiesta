57eedd6b8c8bca805f6fa2b5d5f825dd8e5a40e2
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-28 17:50:44 -0700

    added slogan event listener

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // import functions and grab DOM elements
 const climateSelect = document.getElementById('climate-select');
-const climatePicture = document.getElementById('climate-pic');
+const climatePhoto = document.getElementById('newDay');
 
 const architectureSelect = document.getElementById('architecture-select');
-const architecturePicture = document.getElementById('architecture-pic');
+const architecturePhoto = document.getElementById('buildingArc');
+
+const sloganInput = document.getElementById('slogan-input');
+
 // let state
 let climate = 0;
 let architecture = 0;
@@ -11,15 +14,24 @@ let slogans = [];
 // set event listeners 
 climateSelect.addEventListener('change', () => {
   console.log('changing climate', climateSelect.value);
-
+  climatePhoto.src = `..assets/${climateSelect.value}-climate.png`;
+  climatePhoto.style.backgroundImage = `url('../assets/${climateSelect.value}-climate.png')`;
   climate++;
 })
 
 architectureSelect.addEventListener('change', () => {
   console.log('changing architecture', architectureSelect.value);
-  
+  architecturePhoto.src = `..assets/${climateSelect.value}-climate.png`;
+  architecturePhoto.style.backgroundImage = `url('../assets/${architecturePhoto.value}-architecture.png')`;
   architecture++;
 })
+
+slogan.addEventListener('click', () => {
+  slogan.push
+})
+
+  }
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
