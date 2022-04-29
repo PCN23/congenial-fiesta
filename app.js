c910d403ff4586226bb107fea888d47462c58302
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-29 16:19:15 -0700

    added slogan

--- app.js
+++ app.js
@@ -11,49 +11,59 @@ const coastlinePhoto = document.getElementById ('coastperk');
 const sloganInput = document.getElementById('slogan-input');
 const sloganBtn = document.getElementById('slogan-btn');
 const sloganList = document.getElementById('slogan-list');
+const list = document.getElementById('list');
+const timesChanged = document.getElementById('timesChanged');
 
 // let state
 let climate = 0;
 let architecture = 0;
 let coastline = 0;
 
-let slogan = [];
+let slogans = [];
 // set event listeners 
 climateSelect.addEventListener('change', () => {
     console.log('changing climate', climateSelect.value);
     climatePhoto.src = `./assets/${climateSelect.value}-climate.png`;
     climatePhoto.style.backgroundImage = `url('./assets/${climateSelect.value}-climate.png')`;
     climate++;
+    displaystats();
 });
 
 architectureSelect.addEventListener('change', () => {
     console.log('changing architecture', architectureSelect.value);
     architecturePhoto.src = `./assets/${architectureSelect.value}-architecture.png`;
-    architecturePhoto.style.backgroundImage = `url('./assets/${architecturePhoto.value}-architecture.png')`;
+    architecturePhoto.style.backgroundImage = `url('./assets/${architectureSelect.value}-architecture.png')`;
     architecture++;
+    displaystats();
 });
 
 coastlineSelect.addEventListener('change', () => {
-  console.log('changing coastline', coastlineSelect.value);
-  coastlinePhoto.src = `./assets/${coastlineSelect.value}-coastline.png`;
-  coastlinePhoto.style.backgroundImage = `url('./assets/${coastlinePhoto.value}-coastline.png')`;
-  coastline++;
+    console.log('changing coastline', coastlineSelect.value);
+    coastlinePhoto.src = `./assets/${coastlineSelect.value}-coastline.png`;
+    coastlinePhoto.style.backgroundImage = `url('./assets/${coastlineSelect.value}-coastline.png')`;
+    coastline++;
+    displaystats();
 });
 
 
-sloganInput.addEventListener('click', () => {
-    slogan.push(sloganList.value);
+sloganBtn.addEventListener('click', () => {
+    const newSlogan = sloganInput.value;
+    slogans.push(newSlogan);
+    sloganInput.value = '';
     displaySlogans();
 });
 
 function displaySlogans() {
-    sloganInput.textContent = '';
+    list.textContent = '';
     for (let slogan of slogans) {
         const p = document.createElement ('p');
         p.textContent = slogan;
-        slogan.append(p);
+        list.append(p);
   // get user input
   // use user input to update state 
+        function displaystats() {
+            timesChanged.textContent = `You have changed the climate ${climate} times, the architecture ${architecture} times and the coastline ${coastline} times.`;
+        }
   // update DOM to reflect the new state
     }
 }
